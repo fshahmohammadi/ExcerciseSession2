9c3a4a3e961769f79962ae563289d2f4125f70db
Farhad Shahmohammadi <farhad.fnr@gmail.com>
2012-02-22 18:24:42 +0100

    second phase completed!

--- a.py
+++ a.py
@@ -1,6 +1,15 @@
 import inputdata
 import numpy
+import math
+from scipy.stats.stats import pearsonr
 data = inputdata.raw_scores
+
+def norm2(x, y, A):
+	return numpy.linalg.norm((A[x]-A[y])[numpy.logical_and( (A[x]) > 0, (A[y]) > 0)], 2)
+
+def corr(x, y, A):
+	return pearsonr(A[x][numpy.logical_and( (A[x]) > 0, (A[y]) > 0)], A[x][numpy.logical_and( (A[x]) > 0, (A[y]) > 0)])[0
+
 
 def main():
 	persons = data.keys()
@@ -11,14 +20,12 @@ def main():
 			_papers.add(j);
 	for i in _papers:
 		papers.append(i)
-	A = numpy.ndarray((len(persons), len(papers)), dtype=float) 
+	A = numpy.zeros((len(persons), len(papers)), dtype=float) 
+	
 	for i, j in data.iteritems():
 		for k, l in j.iteritems():
 			A[persons.index(i)][papers.index(k)] = l
-	for i in range(len(persons)):
-		for j in range(len(papers)):
-			print str(persons[i]) + " " + str(papers[j]) + " " + str(A[i][j])
-
+	
 main()
 
 
